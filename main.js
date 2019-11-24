300b7c57c2d660bf4dc537cef93e3edcea36005e
PawelBlazewicz <52447280+PawelBlazewicz@users.noreply.github.com>
2019-11-24 21:35:12 +0100

    style fix

--- main.js
+++ main.js
@@ -45,17 +45,18 @@ const makeWeatherBox = async id => {
     forecast.innerHTML = `
         <div class="topInfo">
             <div class="city">
-                <h1 class="cityName">${data.city.name}</h1>
+                <h1 class="cityName">
+                    ${data.city.name}
+                </h1>
                 <div class="additionalInfo">
-
-                <span>💦 ${data.list[0].main.humidity}%</span> <span>💨${data.list[0].wind.speed}m/s</span>
-                <span>${data.list[0].main.pressure}hPa</span>
+                    <span>💦 ${data.list[0].main.humidity}%</span>
+                    <span>💨${data.list[0].wind.speed}m/s</span>
+                    <span>${data.list[0].main.pressure}hPa</span>
                 </div>
             </div>
-            <div class="actualTemperature">
-                ${Math.round(data.list[0].main.temp)}°
+            <div class="actualTemperature">               
                 <img class="bigWeatherIcon" src="http://openweathermap.org/img/wn/${data.list[0].weather[0].icon}@2x.png" alt="${data.list[0].weather[0].description}" title="${data.list[0].weather[0].description}">
-
+                ${Math.round(data.list[0].main.temp)}°
             </div>
         </div>
         <div class="bottomInfo"></div>`;
@@ -66,10 +67,11 @@ const makeWeatherBox = async id => {
         const temp = Math.round(i.main.temp);
         const hour = /\s.{5}/.exec(i.dt_txt);
         div.innerHTML = `       
-        <img class="weatherIcon" src="http://openweathermap.org/img/w/${i.weather[0].icon}.png" alt="${i.weather[0].description}" title="${i.weather[0].description}">
-        <div class="tempBar" style="top:${temp>0 ?50 - (temp+5) : 90}px; height:${Math.abs(temp)+5}px;"></div>
-       <div class="timeAndTemp" >${temp}°C
-       <br> ${hour}</div>
+            <img class="weatherIcon" src="http://openweathermap.org/img/w/${i.weather[0].icon}.png" alt="${i.weather[0].description}" title="${i.weather[0].description}">
+            <div class="tempBar" style="top:${temp>0 ?50 - (temp+5) : 90}px; height:${Math.abs(temp)+5}px;"></div>
+            <div class="timeAndTemp" >
+                ${temp}°C <br> ${hour}
+            </div>
         `;
         div.classList.add("temp");
         forecast.children[1].appendChild(div);
